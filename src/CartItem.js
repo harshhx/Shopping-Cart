d9f681216c8df8da323e75414fe6c2fa44836679
harshhx <hg070401@gmail.com>
2021-06-28 00:19:05 +0530

    working increase and decrease button

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,27 +1,38 @@
-import React from'react' ;
+import React from 'react' ;
 
-class CartItem extends React.Component{
-    constructor() {
-        super();
+class CartItem extends React.Component {
+    constructor(props) {
+        super(props);
         this.state = {
             price: 999,
             title: 'Mobile Phone',
             qty: 1,
-            img : '',
+            img: '',
         }
     }
-    increaseQty = () =>{
+
+    increaseQty = () => {
         console.log(this.state.qty);
+        this.setState({
+            qty: this.state.qty + 1
+        });
     }
-    render (){
-        const {price,title,qty} = this.state
-        return(
+
+    decreaseQty = () => {
+        this.setState({
+            qty: this.state.qty - 1
+        });
+    }
+
+    render() {
+        const {price, title, qty} = this.state
+        return (
             <div className="cart-item">
                 <div className="left-block">
-                    <img style={styles.image} />
+                    <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{fontSize: 25}}>{title}</div>
                     <div style={{color: '#77'}}>Rs {price}</div>
                     <div style={{color: '#77'}}>Qty {qty}</div>
                     <div className="cart-item-actions">
@@ -34,8 +45,10 @@ class CartItem extends React.Component{
                         />
                         <img
                             src="https://image.flaticon.com/icons/png/512/992/992683.png"
-                             alt="decrease"
-                            className='action-icons'/>
+                            alt="decrease"
+                            className='action-icons'
+                            onClick={this.decreaseQty}
+                        />
                         <img
                             src="https://image.flaticon.com/icons/png/512/1214/1214428.png"
                             alt="delete"
