2da1726e3c69db93d7d1979385bff72459868ba4
harshhx <hg070401@gmail.com>
2021-06-28 00:50:18 +0530

    follow up on cart component

--- src/Cart.js
+++ src/Cart.js
@@ -4,7 +4,7 @@ import CartItem from "./CartItem";
 class Cart extends React.Component {
     render() {
         return (
-            <div><CartItem/>
+            <div className={"cart"}><CartItem/>
                 <CartItem/>
                 <CartItem/>
             </div>
